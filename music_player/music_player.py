6a2ccaa2de6ff3e1082be53905ce8fb0c6fc0216
Rahul <rahulpbhoir@outlook.com>
2021-04-18 18:13:11 +0530

    v1.1.0 - track list, scroll bar,

--- music_player/music_player.py
+++ music_player/music_player.py
@@ -1,68 +1,106 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import StringVar, filedialog
+from tkinter.constants import ACTIVE, BOTH, END, RIGHT
 from pygame import mixer
-import re
+import os
 
-HEIGHT = 0
-WIDTH = 0
-BUTTON_FONT = ('arial', 18, 'bold')
-LABEL_FONT = ('arial', 18, 'bold')
+WIDTH = 70
+HEIGHT = 50
+FONT_STYLE = 'consolas'
+FONT_SIZE = 18
+BUTTON_FONT = (FONT_STYLE, FONT_SIZE, 'bold')
+LABEL_FONT = (FONT_STYLE,  FONT_SIZE, 'bold')
 
 
 class MusicPlayer():
     def __init__(self, root):
-        root.geometry('300x220')
+        root.geometry('600x200')
         root.resizable(0, 0)
         root.title('Music Player')
 
-        self.label_btn = tk.Label(root, text='', font=LABEL_FONT,
-                                  bg='light blue', width=20, height=2, justify='center')
-        self.label_btn.grid(columnspan=6)
-
-        self.load_btn = tk.Button(root, text='load', font=BUTTON_FONT, width=WIDTH,
-                                  height=HEIGHT, command=self.loadSong)
-        self.load_btn.grid(row=1, column=1)
-
-        self.play_btn = tk.Button(root, text='play', font=BUTTON_FONT, width=WIDTH,
-                                  height=HEIGHT, command=self.playSong)
-        self.play_btn.grid(row=2, column=0)
-
-        self.pause_btn = tk.Button(root, text='pause', font=BUTTON_FONT, width=WIDTH,
-                                   height=HEIGHT, command=self.pauseSong)
-        self.pause_btn.grid(row=2, column=2)
-
-        self.stop_btn = tk.Button(root, text='stop', font=BUTTON_FONT, width=WIDTH,
-                                  height=HEIGHT, command=self.stopSong)
-        self.stop_btn.grid(row=3, column=1)
-
-        self.music_file = False
+        self.music_folder_path = ''
+        self.music_file_name = StringVar()
         self.song_state = False
+        self.music_files = []
 
-    def songName(self):
-        if self.music_file:
-            file_name = str(self.music_file).split('/')[-1].split('.mp3')[0]
-            self.label_btn['text'] = file_name
+        # for displaying song name
+        self.track_name = tk.Label(root, text='Welcome!', font=LABEL_FONT,
+                                   bg='light blue', justify='center')
+        self.track_name.place(x=0, y=0, width=400, height=100)
+
+        # load button
+        self.load_btn = tk.Button(root, text='load', font=BUTTON_FONT,
+                                  command=self.loadSong)
+        self.load_btn.place(x=300, y=120, width=WIDTH, height=HEIGHT)
+
+        # play button
+        self.play_btn = tk.Button(root, text='play', font=BUTTON_FONT,
+                                  command=self.playSong)
+        self.play_btn.place(x=10, y=120, width=WIDTH, height=HEIGHT)
+
+        # pause button
+        self.pause_btn = tk.Button(
+            root, text='pause', font=BUTTON_FONT, command=self.pauseSong)
+        self.pause_btn.place(x=90, y=120, width=WIDTH+30, height=HEIGHT)
+
+        # stop button
+        self.stop_btn = tk.Button(
+            root, text='stop', font=BUTTON_FONT, command=self.stopSong)
+        self.stop_btn.place(x=200, y=120, width=WIDTH, height=HEIGHT)
+
+        # labe frame for track list
+        self.song_list_label_frame = tk.LabelFrame(
+            root, text='track list', font=(FONT_STYLE, 12))
+        self.song_list_label_frame.place(
+            x=400, y=0, width=200, height=200)
+
+        # track list box
+        self.song_list_box = tk.Listbox(
+            self.song_list_label_frame, selectmode='SINGLE')
+        self.song_list_box.bind('<Button-1>', self.playSong)
+        self.song_list_box.place(x=400, y=0, width=200, height=200)
+
+        # scroll bar for track list
+        self.scroll_bar = tk.Scrollbar(root)
+        self.scroll_bar.pack(side=RIGHT, fill=BOTH)
+        self.song_list_box.config(yscrollcommand=self.scroll_bar.set)
+        self.scroll_bar.config(command=self.song_list_box.yview)
 
     def loadSong(self):
-        self.music_file = filedialog.askopenfile()
-        self.songName()
+        # getting the music folder path
+        self.music_folder_path = filedialog.askdirectory() + '/'
+        # getting the track list from the music folder
+        self.music_files = os.listdir(self.music_folder_path)
+        # displaying the track list in the list box
+        for file in self.music_files:
+            self.song_list_box.insert(END, file)
 
     def playSong(self):
+        # to get the song name from the songs list
+        self.music_file = self.song_list_box.get(ACTIVE)
+        # displaying song name in the label box
+        self.track_name['text'] = self.music_file
+        # playing the song
         if self.music_file:
             mixer.init()
-            mixer.music.load(self.music_file)
+            mixer.music.load(self.music_folder_path + self.music_file)
             mixer.music.play()
             self.song_state = True
 
     def pauseSong(self):
         if self.song_state:
             mixer.music.pause()
+            # for changing the button text to unpause
+            self.pause_btn['text'] = 'unpause'
             self.song_state = False
         else:
             mixer.music.unpause()
+            # for changing the button text to pause
+            self.pause_btn['text'] = 'pause'
             self.song_state = True
 
     def stopSong(self):
+        # stop the song
         mixer.music.stop()
         self.song_state = False
 
